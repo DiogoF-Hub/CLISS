7fd6bcfb22359571f3c050e7e7f658a1d280996e
DiogoF-Hub <60687275+DiogoF-Hub@users.noreply.github.com>
2021-05-19 23:28:15 +0200

    h vfg

--- 3TPIFI/CLISS/12.05.2021/picture time/picture.js
+++ 3TPIFI/CLISS/12.05.2021/picture time/picture.js
@@ -1,26 +1,34 @@
-let images = ["dog.png", "cat.png", "fish.png", "horse.png"]
+let images = ["cat.png", "cat.png", "dog.png", "dog.png", "fish.png", "fish.png", "horse.png", "horse.png"];
+
+let backcardimg = document.createElement("img");
+    backcardimg.src = "backcard.png";
+    backcardimg.alt = "backcard";
+    backcardimg.classList.add("img");
+    backcardimg.addEventListener("click", imageclicked);
+
+function Start() {
+    for(let i=0;i<images.length; i++){
+        document.getElementById("").append(backcardimg);
+    }
+}
+
 
 function imageclicked(){
     let mydogimg = document.createElement("img");
     mydogimg.src = "dog.png";
     mydogimg.alt = "dog";
     mydogimg.classList.add("img");
-    document.getElementById("divcat").innerHTML = " ";
-    document.getElementById("divcat").append(mydogimg);
+    document.getElementById("divAllCards").innerHTML = " ";
+    document.getElementById("divAllCards").append(mydogimg);
 
     setTimeout(putimageback, 4000)
 }
 
 function putimageback(){
-    let backcardimg = document.createElement("img");
-    backcardimg.src = "backcard.png";
-    backcardimg.alt = "backcard";
-    backcardimg.classList.add("img");
-    backcardimg.addEventListener("click", imageclicked);
     
-    document.getElementById("divcat").innerHTML = " ";
+    document.getElementById("divAllCards").innerHTML = " ";
 
-    document.getElementById("divcat").append(backcardimg);
+    document.getElementById("divAllCards").append(backcardimg);
 }
 
 //let mycatimg = document.createElement("img");
