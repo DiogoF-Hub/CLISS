d110da3afb2455cb1d7e6e57e35de8df04f577d2
DiogoF-Hub <60687275+DiogoF-Hub@users.noreply.github.com>
2021-06-01 21:00:30 +0200

    dd

--- 3TPIFI/CLISS/12.05.2021/picture time/picture.js
+++ 3TPIFI/CLISS/12.05.2021/picture time/picture.js
@@ -1,37 +1,42 @@
-let images = ["cat.png", "cat.png", "dog.png", "dog.png", "fish.png", "fish.png", "horse.png", "horse.png"];
+let PokemonImages = ["Pikachu.png", "Charmeleon.png", "Caterpie.png", "Shiny.png", "Lugia.png", "Shiggy.png", "Ghost.png", "Gengar.png", "Greninja.png", "Onix.png", "Toxiped.png", "Bulbasaur.png"]
 
-let backcardimg = document.createElement("img");
-    backcardimg.src = "backcard.png";
-    backcardimg.alt = "backcard";
-    backcardimg.classList.add("img");
-    backcardimg.addEventListener("click", imageclicked);
+let arrayOfCardIds = [];
+
+let CardRevealed = 0;
+
+let isTheGameOn = false;
+
 
 function Start() {
-    for(let i=0;i<images.length; i++){
-        document.getElementById("").append(backcardimg);
+    for (let i = 0; i < PokemonImages.length; i++) {
+        let newImage = document.createElement("img");
+        newImage.src = "CardBack.png";
+        newImage.id = "CardNo" + i;
+        arrayOfCardIds.push(newImage.id);
+        newImage.classList.add("oneCard");
+        document.getElementById("myGame").append(newImage);
+        newImage.addEventListener("click", function () { cardClicked(newImage.id, i); });  //Q try to change this
     }
 }
 
+function cardClicked(idOfClickedCard, PokemonIndex) {
+    if (CardRevealed != 2) {
+        document.getElementById(idOfClickedCard).src = PokemonImages[PokemonIndex];
+        CardRevealed++;
+        var cardClickedAudio = new Audio('Adúlti_Xupa_Cabra.mp3');
+        cardClickedAudio.play();
+    }
 
-function imageclicked(){
-    let mydogimg = document.createElement("img");
-    mydogimg.src = "dog.png";
-    mydogimg.alt = "dog";
-    mydogimg.classList.add("img");
-    document.getElementById("divAllCards").innerHTML = " ";
-    document.getElementById("divAllCards").append(mydogimg);
-
-    setTimeout(putimageback, 4000)
+    if (CardRevealed == 2) {
+        setTimeout(function () {
+            putImageBack();   //Q try to change this
+        }, 1500);
+    }
 }
 
-function putimageback(){
-    
-    document.getElementById("divAllCards").innerHTML = " ";
-
-    document.getElementById("divAllCards").append(backcardimg);
+function putImageBack() {
+    for (CardIds of arrayOfCardIds) {
+        document.getElementById(CardIds).src = "CardBack.png";
+        CardRevealed = 0;
+    }
 }
-
-//let mycatimg = document.createElement("img");
-//mycatimg.src = "cat.png";
-//mycatimg.alt = "cat";
-//mycatimg.addEventListener("click", imageclicked)
