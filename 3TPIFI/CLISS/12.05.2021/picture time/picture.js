9f2250a1bb6344d2dc5c3a26d1f261c0f80e4ccc
DiogoF-Hub <60687275+DiogoF-Hub@users.noreply.github.com>
2021-06-03 23:19:25 +0200

    dd

--- 3TPIFI/CLISS/12.05.2021/picture time/picture.js
+++ 3TPIFI/CLISS/12.05.2021/picture time/picture.js
@@ -1,13 +1,44 @@
-let PokemonImages = ["Pikachu.png", "Charmeleon.png", "Caterpie.png", "Shiny.png", "Lugia.png", "Shiggy.png", "Ghost.png", "Gengar.png", "Greninja.png", "Onix.png", "Toxiped.png", "Bulbasaur.png"]
+let PokemonImages = ["Pikachu.png", "Charmeleon.png", "Caterpie.png", "Shiny.png", "Lugia.png", "Shiggy.png", "Ghost.png", "Gengar.png", "Greninja.png", "Onix.png", "Toxiped.png", "Bulbasaur.png", "gyarados.png", "Pikachu.png", "Charmeleon.png", "Caterpie.png", "Shiny.png", "Lugia.png", "Shiggy.png", "Ghost.png", "Gengar.png", "Greninja.png", "Onix.png", "Toxiped.png", "Bulbasaur.png", "gyarados.png"]
+
+let ShuffledPokemonImages = [];
 
 let arrayOfCardIds = [];
 
+let firstCardRevealed = 0;
+
 let CardRevealed = 0;
 
-let isTheGameOn = false;
+let lifes = 30;
 
+//let mode = hard;
+
+/*function Easymode() {
+    mode = easy;
+    document.getElementById("buttonmode").innerHTML = "Hard Mode";
+    Start();
+}
+
+function Hardmode() {
+    mode = hard;
+    document.getElementById("buttonmode").innerHTML = "Easy Mode";
+    Start();
+}*/
 
 function Start() {
+
+    document.getElementById("lifesId").innerHTML = "Lifes left: " + lifes;
+
+    //if (mode == hard) {
+        for (; PokemonImages.length > 0;) {
+            let randomPokemonImage = Math.floor(Math.random() * PokemonImages.length);
+            ShuffledPokemonImages.push(PokemonImages[randomPokemonImage])
+            PokemonImages.splice(randomPokemonImage, 1)
+
+        }
+        PokemonImages = ShuffledPokemonImages;
+    //}
+
+
     for (let i = 0; i < PokemonImages.length; i++) {
         let newImage = document.createElement("img");
         newImage.src = "CardBack.png";
@@ -20,23 +51,62 @@ function Start() {
 }
 
 function cardClicked(idOfClickedCard, PokemonIndex) {
-    if (CardRevealed != 2) {
+
+
+    if (idOfClickedCard != firstCardRevealed && CardRevealed != 2) {
         document.getElementById(idOfClickedCard).src = PokemonImages[PokemonIndex];
         CardRevealed++;
-        var cardClickedAudio = new Audio('Quack_Sound_Effect.mp3');
+        if (CardRevealed == 1) firstCardRevealed = idOfClickedCard;
+
+        var cardClickedAudio = new Audio('Pokemon_(A_Button).mp3');
         cardClickedAudio.play();
+
+        if (CardRevealed == 2) {
+            let imageNameOffFirstCard = document.getElementById(firstCardRevealed).src;
+            let imagesNameOffSecondCard = document.getElementById(idOfClickedCard).src;
+            if (imageNameOffFirstCard == imagesNameOffSecondCard) {
+                for (let i = 0; i < arrayOfCardIds.length; i++) {
+                    if (arrayOfCardIds[i] == firstCardRevealed) {
+                        arrayOfCardIds.splice(i, 1)
+                        i--;
+                    }
+
+                    if (arrayOfCardIds[i] == idOfClickedCard) {
+                        arrayOfCardIds.splice(i, 1)
+                        i--;
+                    }
+                }
+
+
+                lifes++;
+            }
+
+
+            lifes--;
+            document.getElementById("lifesId").innerHTML = "Lifes left: " + lifes;
+
+            setTimeout(function () {
+                putAllImageBack();   //Q try to change this
+            }, 1500);
+        }
     }
 
-    if (CardRevealed == 2) {
-        setTimeout(function () {
-            putImageBack();   //Q try to change this
-        }, 1500);
-    }
 }
 
-function putImageBack() {
+function putAllImageBack() {
+
+    if (arrayOfCardIds.length == 0) {
+        window.location.href = 'Win.html';
+    }
+
+    if (lifes == 0) {
+        window.location.href = 'GameOver.html';
+    }
+
     for (CardIds of arrayOfCardIds) {
         document.getElementById(CardIds).src = "CardBack.png";
+        firstCardRevealed = 0;
         CardRevealed = 0;
     }
 }
+
